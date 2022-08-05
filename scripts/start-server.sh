cb00f0d6c09d0cfcd1cb63116bf7a238cc323640
ich777 <christophhummer@gmail.com>
2022-08-05 09:05:58 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -51,6 +51,15 @@ elif [ "${CUR_V}" == "${LAT_V}" ]; then
 	echo "---MyCrypto v$CUR_V up-to-date---"
 fi
 
+if [ "${CUSTOM_RES_W}" -le 1279 ]; then
+	echo "---Width to low must be a minimal of 1280 pixels, correcting to 1024...---"
+    CUSTOM_RES_W=1280
+fi
+if [ "${CUSTOM_RES_H}" -le 1023 ]; then
+	echo "---Height to low must be a minimal of 1024 pixels, correcting to 768...---"
+    CUSTOM_RES_H=1024
+fi
+
 echo "---Checking for old display lock files---"
 rm -rf /tmp/.X99*
 rm -rf /tmp/.X11*
