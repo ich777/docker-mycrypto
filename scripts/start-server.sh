3737189c65530cd208b8687357b5b4c0681d3817
ich777 <christophhummer@gmail.com>
2022-08-05 09:04:17 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,6 +2,54 @@
 export DISPLAY=:99
 export XAUTHORITY=${DATA_DIR}/.Xauthority
 
+LAT_V="$(wget -qO- https://api.github.com/repos/MyCryptoHQ/MyCrypto/releases/latest | jq -r '.tag_name')"
+CUR_V="$(find ${DATA_DIR}/bin -maxdepth 1 -type f -name "mycryptov_*" 2>/dev/null | cut -d '_' -f2)"
+
+if [ -z "${LAT_V}" ]; then
+  if [ -z "${CUR_V}" ]; then
+    echo "---Can't get latest version from MyCrypto and found no local installed version!---"
+	sleep infinity
+  else
+    echo "---Can't get latest version from MyCrypto, falling back to installed version ${CUR_V}---"
+	LAT_V="${CUR_V}"
+  fi
+fi
+
+echo "---Version Check---"
+if [ -z "${CUR_V}" ]; then
+  echo "---MyCrypto not installed, installing...---"
+  cd ${DATA_DIR}
+  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/mycrypto_${LAT_V}.AppImage "https://github.com/MyCryptoHQ/MyCrypto/releases/download/${LAT_V}/linux-x86-64_${LAT_V}_MyCrypto.AppImage" ; then
+    echo "---Sucessfully downloaded MyCrypto---"
+  else
+    echo "---Something went wrong, can't download MyCrypto, putting container in sleep mode---"
+    sleep infinity
+  fi
+  chmod +x ${DATA_DIR}/mycrypto_${LAT_V}.AppImage
+  ${DATA_DIR}/mycrypto_${LAT_V}.AppImage --appimage-extract
+  mkdir -p ${DATA_DIR}/bin
+  mv $(find ${DATA_DIR}/squashfs-root -type d -name "app")/ ${DATA_DIR}/bin
+  rm -rf ${DATA_DIR}/squashfs-root ${DATA_DIR}/mycrypto_${LAT_V}.AppImage
+  touch ${DATA_DIR}/bin/mycryptov_${LAT_V}
+elif [ "${CUR_V}" != "${LAT_V}" ]; then
+  echo "---Version missmatch, installed v${CUR_V}, downloading and installing latest v${LAT_V}...---"
+  cd ${DATA_DIR}
+  rm -rf ${DATA_DIR}/bin
+  if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${DATA_DIR}/mycrypto_${LAT_V}.AppImage "https://github.com/MyCryptoHQ/MyCrypto/releases/download/${LAT_V}/linux-x86-64_${LAT_V}_MyCrypto.AppImage" ; then
+    echo "---Sucessfully downloaded MyCrypto---"
+  else
+    echo "---Something went wrong, can't download MyCrypto, putting container in sleep mode---"
+    sleep infinity
+  fi
+  chmod +x ${DATA_DIR}/mycrypto_${LAT_V}.AppImage
+  ${DATA_DIR}/mycrypto_${LAT_V}.AppImage --appimage-extract
+  mkdir -p ${DATA_DIR}/bin
+  mv $(find ${DATA_DIR}/squashfs-root -type d -name "app")/ ${DATA_DIR}/bin
+  rm -rf ${DATA_DIR}/squashfs-root ${DATA_DIR}/mycrypto_${LAT_V}.AppImage
+  touch ${DATA_DIR}/bin/mycryptov_${LAT_V}
+elif [ "${CUR_V}" == "${LAT_V}" ]; then
+	echo "---MyCrypto v$CUR_V up-to-date---"
+fi
 
 echo "---Checking for old display lock files---"
 rm -rf /tmp/.X99*
@@ -28,3 +76,4 @@ sleep infinity
 
 echo "---Starting MyCrypto---"
 cd ${DATA_DIR}
+${DATA_DIR}/mycrypto
